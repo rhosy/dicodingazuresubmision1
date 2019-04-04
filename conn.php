55ed3e8bac6741561acb7d2d7c335e0b0ced7457
Khoirur Rosy <khoirur.rosy@gmail.com>
2019-04-04 16:13:11 +0700

    replace code

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-//MySQLi Procedural
-$conn = mysqli_connect("sqlsrv:server = tcp:rosydbserver.database.windows.net,1433; Database = rosydicodingsubmission","rosy","@rhosytux18");
-if (!$conn) {
-	die("Koneksi Gagal: " . mysqli_connect_error());
-}
- 
-?>
--- delete.php
+++ delete.php
@@ -1,7 +1,15 @@
 <?php
-	include('conn.php');
-	$id=$_GET['id'];
-	mysqli_query($conn,"delete from user where userid='$id'");
-	header('location:index.php');
+//including the database connection file
+include("config.php");
 
+//getting id of the data from url
+$id = $_GET['id'];
+
+//deleting the row from table
+$sql = "DELETE FROM users WHERE id=:id";
+$query = $dbConn->prepare($sql);
+$query->execute(array(':id' => $id));
+
+//redirecting to the display page (index.php in our case)
+header("Location:index.php");
 ?>
