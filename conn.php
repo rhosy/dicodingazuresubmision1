8ddfd20edcebec8d37c9b90762c33b7d5ffbfb7c
Khoirur Rosy <khoirur.rosy@gmail.com>
2019-04-04 16:05:53 +0700

    config db to azure

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("sqlsrv:server = tcp:rosydbserver.database.windows.net,1433","rosy","@rhosytux18","rosydicodingsubmission");
+$conn = mysqli_connect("sqlsrv:server = tcp:rosydbserver.database.windows.net,1433; Database = rosydicodingsubmission","rosy","@rhosytux18");
 if (!$conn) {
 	die("Koneksi Gagal: " . mysqli_connect_error());
 }
