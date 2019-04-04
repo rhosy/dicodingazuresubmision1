43f3b37746e1fdd0e992d093732382eb95076cfe
Khoirur Rosy <khoirur.rosy@gmail.com>
2019-04-04 15:58:26 +0700

    change server db

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("sqlsrv:server = tcp:rosydbserver.database.windows.net","rosy","@rhosytux18","rosydicodingsubmission");
+$conn = mysqli_connect("sqlsrv:server = tcp:rosydbserver.database.windows.net,1433","rosy","@rhosytux18","rosydicodingsubmission");
 if (!$conn) {
 	die("Koneksi Gagal: " . mysqli_connect_error());
 }
