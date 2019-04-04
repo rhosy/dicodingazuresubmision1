3d7378593b8e6a88a2d87b9947d13e69763cd0ca
Khoirur Rosy <khoirur.rosy@gmail.com>
2019-04-04 16:45:54 +0700

    change connection to db

--- config.php
+++ config.php
@@ -1,13 +1,7 @@
 <?php
-
-$databaseHost = 'rosydbserver.database.windows.net,1433';
-$databaseName = 'rosydicodingsubmission';
-$databaseUsername = 'rosy';
-$databasePassword = '@rhosytux18';
-
 try {
 	// http://php.net/manual/en/pdo.connections.php
-	$dbConn = new PDO("sqlsrv:server={$databaseHost};Database={$databaseName}", $databaseUsername, $databasePassword);
+	$dbConn = new PDO("sqlsrv:server = tcp:rosydbserver.database.windows.net,1433; Database = rosydicodingsubmission", "rosy", "@rhosytux18");
 	
 	$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Setting Error Mode as Exception
 	// More on setAttribute: http://php.net/manual/en/pdo.setattribute.php
